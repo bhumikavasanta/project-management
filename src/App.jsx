7bef197dd4763e59b31e159ae175ede0f35d8237
Bhumika Vasanta <bhumikavasanta2460@gmail.com>
2024-12-18 18:44:19 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ProjectsSideBar from './components/ProjectsSideBar';
+import './index.css';
+
+function App() {
+  return (
+    <main className='h-screen my-8'>
+      <ProjectsSideBar />
+    </main>
+  );
+}
+
+export default App;
