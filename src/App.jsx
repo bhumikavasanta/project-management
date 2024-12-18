f98accc8482c8f615d12ab0afca11bbabb208bdf
Bhumika Vasanta <bhumikavasanta2460@gmail.com>
2024-12-18 20:23:22 +0530

    Added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
+import { useState } from 'react';
+import NewProject from './components/NewProject';
+import NoProjectSelected from './components/NoProjectSelected';
 import ProjectsSideBar from './components/ProjectsSideBar';
 import './index.css';
 
 function App() {
+
+  const [projectState, setProjectState] = useState({
+    selectedProject: undefined,
+    projects: []
+  })
+
+  const handleStartAddProject = () => {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProject: null,
+      }
+    })
+  }
+
+  let content;
+  if(projectState.selectedProject === null) {
+    content = <NewProject />
+  } else if(projectState.selectedProject === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+  }
+
   return (
-    <main className='h-screen my-8'>
+    <main className='h-screen my-8 flex gap-8'>
       <ProjectsSideBar />
+      {content}
     </main>
   );
 }
